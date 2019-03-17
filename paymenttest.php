b8bc057ce1525d1429965bc58995dd071a85ec41
Sukanth GC <sukanth1998@gmail.com>
2019-03-17 13:18:08 +0530

    Create paymenttest.php

---
+++ paymenttest.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+<div class='pm-button'><a href='https://www.payumoney.com/paybypayumoney/#/C5D6884E80C34EC0CECC9F97327F4ACF'><img src='https://www.payumoney.com/media/images/payby_payumoney/new_buttons/22.png' /></a></div> 
+</body>
+</html>
